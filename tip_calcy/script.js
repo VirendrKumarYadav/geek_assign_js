84cf9fe7d72d36437331205b6a39d04c67d7df92
Virendra Yadav <softe@Suraj@gmail.com>
2023-10-12 09:52:12 +0530

    aading all the cchanges

--- tip_calcy/script.js
+++ tip_calcy/script.js
@@ -1,9 +1,41 @@
 
-document.getElementById('in').addEventListener('input', function (){
-   let txt=document.querySelector('#in').value;
-   console.log(txt);
-   let prt=document.querySelector('#print');
-   console.log(prt.);
-//   textarae.innerText=document.getElementById('in').value;
-// console.log(typeof document.getElementById('in').value);
+const bill_amt=document.querySelector('#bill_amt');
+const tip_am=document.querySelector('#tip_amt');
+const noOfPpl=document.querySelector('#noOfPeople');
+const total_tip=document.querySelector('.total_tip_amt');
+const total_amt=document.querySelector('.total_amt');
+const calculate_btn=document.querySelector('#calcy');
+const clr_btn=document.querySelector('#clr');
+console.log(bill_amt+" "+tip_am+" "+clr_btn);
+// console.log(total_amt);
+
+let bill=0, tip=0, ppl=0;
+bill_amt.addEventListener('input', (amt)=>{
+   bill=parseInt(amt.target.value);
+});
+
+tip_am.addEventListener('input', (amt1)=>{
+   tip=parseInt(amt1.target.value);
+   // console.log(tip);
+});
+noOfPpl.addEventListener('input', (amt2)=>{
+   ppl=parseInt(amt2.target.value);
+});
+
+calculate_btn.addEventListener('click', (a)=>{
+   // console.log(a.target.value);
+   tip=(tip*bill);
+   tip=tip/100;
+   // console.log(tip);
+   total_tip.textContent=tip/ppl;
+   total_amt.textContent=tip+bill;
+});
+clr_btn.addEventListener('click', ()=>{
+   console.log(total_amt.textContent);
+   bill_amt.target.value=`0`;
+   tip_am.target.value=`0`;
+   noOfPpl.target.value=`0`;
+   total_tip.textContent='00';
+   total_amt.textContent='00';
+
 });
