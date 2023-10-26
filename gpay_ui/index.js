93414577dc5729b1e37cdb6708ec21ce34411e81
Virendra Yadav <softe@Suraj@gmail.com>
2023-10-26 08:36:40 +0530

    aading all the cchanges

--- gpay_ui/index.js
+++ gpay_ui/index.js
@@ -1,5 +1,18 @@
 let next=document.querySelector('.next');
-console.log(next);
+let amt=document.querySelector('.pay input');
+let note=document.querySelector('.note');
+// let pay_ppAmt=document.querySelector('.pay_btn span');
+// console.log(pay_ppAmt);
 next.addEventListener('click', ()=>{
+localStorage.setItem("Amount",amt.value);
+pay_ppAmt.innertext=amt.value;
 
-})
+});
+
+
+next.addEventListener('click', ()=>{
+    localStorage.setItem("Note",note.value);
+    });
+
+
+    
